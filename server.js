d1ffe005462b73879bb19cbc93edd9b690039779
amit pathak <amitaxondelhi@gmail.com>
2026-05-03 17:21:12 +0530

    fix render port

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
